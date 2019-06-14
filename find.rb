7f34470d20e8d6cb1dc7e3f96b41180ec005174b
Linda Goldstein <lindag@squareup.com>
2019-06-14 11:01:24 -0700

    WIP learning to use OptionParser

--- find.rb
+++ find.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+
+  opts.on("-name N", "--name N ", "Files matching pattern") do |pattern|
+    options[:match_pattern] = pattern
+  end
+end.parse!
+
+p "OPTIONS"
+p options
+
+p "ARGV"
+p ARGV
 args = ARGV
 require_relative 'lib/finder'
 puts Finder.new(args).call
